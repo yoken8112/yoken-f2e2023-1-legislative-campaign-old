6c9f64cc7b50aaaf8ff6daf37968d7cce865a499
Yoken <yoken@whygo.com.tw>
2023-11-19 12:29:13 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     base: '/yoken-f2e2023-1-legislative-campaign/',
-    publicPath: '/yoken-f2e2023-1-legislative-campaign/',
+    publicPath: '/yoken-f2e2023-1-legislative-campaign/dist/',
     plugins: [
         vue()
     ]
