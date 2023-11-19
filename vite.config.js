d25c390200f9d428268d55f4f0a3a48e95f15db3
Yoken <yoken@whygo.com.tw>
2023-11-19 22:20:21 +0800

    change name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/yoken-deploy-sample/",
+  base: "/yoken-f2e2023-1-legislative-campaign/",
   plugins: [
     vue(),
   ],
